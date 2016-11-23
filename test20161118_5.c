eafe8e5978818eecbcb97bcb121bee732630bc52
Foad Sojoodi Farimani <foadsf@Foads-MacBook-Pro.local>
2016-11-23 22:53:00 +0100

    some tests were added

---
+++ test20161118_5.c
@@ -0,0 +1,44 @@
+#include "intlist.h"
+
+int main(int argc, char const *argv[]) {
+
+  intlist tmplist1;
+
+  addtointlist(&tmplist1, 4);
+
+  addtointlist(&tmplist1, 4);
+
+  addtointlist(&tmplist1, 5);
+  addtointlist(&tmplist1, 6);
+  addtointlist(&tmplist1, 8);
+  addtointlist(&tmplist1, 23);
+  addtointlist(&tmplist1, 11);
+  printintlist(tmplist1);
+
+  intlist tmplist2;
+
+  addtointlist(&tmplist2, 3);
+  printintlist(tmplist2);
+  addtointlist(&tmplist2, 12);
+
+  addtointlist(&tmplist2, 8);
+  addtointlist(&tmplist2, 98);
+  addtointlist(&tmplist2, 24);
+  addtointlist(&tmplist2, 32);
+  addtointlist(&tmplist2, 1);
+  printintlist(tmplist2);
+
+  // rmfrmintlist(&tmplist1, 4);
+  // rmelmintlist(&tmplist1, 4);
+  // printintlist(tmplist1);
+
+  printintlist(appendintlists(tmplist1, tmplist2));
+  printintlist(intpluslist(10, tmplist1));
+  printintlist(intmultlist(10, tmplist1));
+  printintlist(listpluslist(tmplist2, tmplist1));
+  printintlist(listmultlist(tmplist2, tmplist1));
+  insertintlist(&tmplist1, 41, 3);
+  printintlist(tmplist1);
+
+  return 0;
+}
---
+++ test20161122_1.c
@@ -0,0 +1,25 @@
+#include "intlist.h"
+
+int main(int argc, char const *argv[]) {
+
+  intlist tmplist1;
+  tmplist1.l = 8;
+  int intarr1[] = {1, 8, 3, 4, 7, 6, 5, 2};
+  tmplist1.e = &intarr1[0];
+  printintlist(tmplist1);
+
+  intlist tmplist2;
+  tmplist2.l = 8;
+  int intarr2[] = {2, 2, 2, 3, 5, 7, 9, 21};
+  tmplist2.e = &intarr2[0];
+  printintlist(tmplist2);
+
+  intlist tmplist3 = clplsintlist(tmplist1, tmplist2);
+  printintlist(tmplist3);
+
+  intlist tmplist4 = clmltintlist(tmplist1, tmplist2);
+  printintlist(tmplist4);
+
+
+  return 0;
+}
